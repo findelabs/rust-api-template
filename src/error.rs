49f0cd0e949589f5245e91447d46d9f4609e3701
admin <circa.1991@gmail.com>
2024-05-04 16:14:30 -0400

    shifting errors

--- src/error.rs
+++ src/error.rs
@@ -1,32 +1,28 @@
-//use serde_json::error::Error as SerdeError;
+use thiserror::Error;
 use axum::{
-    body::{self},
     http::StatusCode,
-    response::{IntoResponse, Response},
+    response::{IntoResponse, Response}
 };
-use std::fmt;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum Error {
-    Forbidden,
-    Unauthorized,
-    NotFound,
-    OpenTelemetry(opentelemetry::trace::TraceError),
-	TracingErr(tracing::dispatcher::SetGlobalDefaultError),
-}
+	#[error("{{\"error\": \"Cannot get config: Forbidden\"}}")]
+	Forbidden,
+	#[error("{{\"error\": \"Cannot get config: Unauthorized\"}}")]
+	Unauthorized,
+	#[error("{{\"error\": \"Cannot get config: Not found\"}}")]
+	NotFound,
 
-impl std::error::Error for Error {}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::Forbidden => f.write_str("{\"error\": \"Cannot get config: Forbidden\"}"),
-            Error::Unauthorized => f.write_str("{\"error\": \"Cannot get config: Unauthorized\"}"),
-            Error::NotFound => f.write_str("{\"error\": \"Cannot get config: Not found\"}"),
-            Error::OpenTelemetry(ref err) => write!(f, "{}", err),
-			Error::TracingErr(ref err) => write!(f, "{}", err),
-        }
-    }
+	#[error(transparent)]
+    OpenTelemetry {
+        #[from]
+		source: opentelemetry::trace::TraceError
+    },
+	#[error(transparent)]
+	TracingErr {
+		#[from]
+		source: tracing::dispatcher::SetGlobalDefaultError
+	}
 }
 
 impl IntoResponse for Error {
@@ -34,7 +30,7 @@ impl IntoResponse for Error {
         let payload = self.to_string();
         let body = axum::body::Body::new(payload);
 
-		let status_code = match &self {
+        let status_code = match &self {
             Error::Unauthorized | Error::Forbidden => StatusCode::UNAUTHORIZED,
             Error::NotFound => StatusCode::NOT_FOUND,
             _ => StatusCode::INTERNAL_SERVER_ERROR,
@@ -44,17 +40,5 @@ impl IntoResponse for Error {
             .status(status_code)
             .body(body)
             .unwrap()
-    }
-}
-
-impl From<tracing::dispatcher::SetGlobalDefaultError> for Error {
-    fn from(err: tracing::dispatcher::SetGlobalDefaultError) -> Error {
-        Error::TracingErr(err)
-    }
-}
-
-impl From<opentelemetry::trace::TraceError> for Error {
-    fn from(err: opentelemetry::trace::TraceError) -> Error {
-        Error::OpenTelemetry(err)
     }
 }
